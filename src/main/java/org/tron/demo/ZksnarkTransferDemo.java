c7ce20655ad1d358fb15c2fb1b762c164a654372
zergweak <zhang_anwen@163.com>
2018-10-29 17:59:41 +0800

    add testGenKey 25519

--- src/main/java/org/tron/demo/ZksnarkTransferDemo.java
+++ src/main/java/org/tron/demo/ZksnarkTransferDemo.java
@@ -1,7 +1,15 @@
 package org.tron.demo;
 
 import com.google.protobuf.ByteString;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import org.tron.common.utils.ByteArray;
+import org.tron.eddsa.EdDSAPrivateKey;
+import org.tron.eddsa.EdDSAPublicKey;
+import org.tron.eddsa.KeyPairGenerator;
+import org.tron.eddsa.MathUtils;
+import org.tron.eddsa.math.GroupElement;
 import org.tron.protos.Contract.ZksnarkV0TransferContract;
 import org.tron.protos.Protocol.Transaction;
 import org.tron.walletserver.WalletApi;
@@ -19,6 +27,40 @@ public class ZksnarkTransferDemo {
     return null;
   }
 
+  public static void testGenKey(){
+    KeyPairGenerator generator = new KeyPairGenerator();
+    KeyPair keyPairA = generator.generateKeyPair();
+
+    PrivateKey privateKeyA = keyPairA.getPrivate();
+    PublicKey publickeyA = keyPairA.getPublic();
+    GroupElement A = ((EdDSAPublicKey)(publickeyA)).getA();
+    byte[] a = ((EdDSAPrivateKey)(privateKeyA)).geta();
+
+
+    KeyPair keyPairB = generator.generateKeyPair();
+
+    PrivateKey privateKeyB = keyPairB.getPrivate();
+    PublicKey publickeyB = keyPairB.getPublic();
+
+    GroupElement B = ((EdDSAPublicKey)(publickeyB)).getA();
+    byte[] b = ((EdDSAPrivateKey)(privateKeyB)).geta();
+
+
+    GroupElement C = MathUtils.scalarMultiplyGroupElement(B, MathUtils.toFieldElement(MathUtils.toBigInteger(a)));
+    GroupElement C1 = MathUtils.scalarMultiplyGroupElement(A, MathUtils.toFieldElement(MathUtils.toBigInteger(b)));
+  //  GroupElement C = B.scalarMultiply(a);
+   // GroupElement C1 = A.scalarMultiply(b);
+
+    System.out.println(ByteArray.toHexString(A.toByteArray()));
+    System.out.println(ByteArray.toHexString(a));
+    System.out.println(ByteArray.toHexString(B.toByteArray()));
+    System.out.println(ByteArray.toHexString(b));
+    System.out.println(ByteArray.toHexString(C.toByteArray()));
+    System.out.println(ByteArray.toHexString(C1.toByteArray()));
+
+  }
+
   public static void main(String[] args) {
+    testGenKey();
   }
 }
