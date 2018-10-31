1ba478c757b25180f5ebe7be669d5153e40bbd9f
sean-liu55 <liushaohua9017@163.com>
2018-10-31 12:18:41 +0800

    add test

--- src/main/java/org/tron/common/utils/CmUtils.java
+++ src/main/java/org/tron/common/utils/CmUtils.java
@@ -2,34 +2,34 @@ package org.tron.common.utils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import org.tron.demo.ZksnarkTransferDemo;
 
 public class CmUtils {
 
   static private String CM_FILE_NAME = "cmInfoFile.data";
-  static public HashMap<byte[], CmTuple> cmInfoMap = loadCmFile(CM_FILE_NAME);
+  static public HashMap<String, CmTuple> cmInfoMap = new HashMap<>();
 
-  public static HashMap<byte[], CmTuple> loadCmFile(String fileName) {
+  public static void loadCmFile() {
+    cmInfoMap = loadCmFile(CM_FILE_NAME);
+  }
 
-    HashMap<byte[], CmTuple> cmInfoMap = new HashMap<>();
+  public static HashMap<String, CmTuple> loadCmFile(String fileName) {
+
+    HashMap<String, CmTuple> cmInfoMap = new HashMap<>();
     BufferedReader file = null;
     try {
-      InputStream is = ZksnarkTransferDemo.class.getResourceAsStream(fileName);
-      if (is == null) {
+      FileReader fileReader = new FileReader(fileName);
+      if (fileReader == null) {
         throw new IOException("Resource not found: " + fileName);
       }
-      file = new BufferedReader(new InputStreamReader(is));
+      file = new BufferedReader(fileReader);
       String line;
       while ((line = file.readLine()) != null) {
         CmTuple cmTuple = new CmTuple(line);
-        cmInfoMap.put(cmTuple.cm, cmTuple);
+        cmInfoMap.put(cmTuple.getKeyString(), cmTuple);
       }
     } catch (IOException e) {
       e.printStackTrace();
@@ -44,28 +44,30 @@ public class CmUtils {
     return cmInfoMap;
   }
 
-  public static void saveCmFile(String fileName) {
-    FileWriter file = null;
-    try {
-      file = new FileWriter(fileName);
-      BufferedWriter bufWriter = new BufferedWriter(file);
+  public static void saveCmFile() {
+    saveCmFile(CM_FILE_NAME);
+  }
 
-      cmInfoMap.values().forEach(cmTuple -> {
-            try {
-              bufWriter.write(cmTuple.toLine());
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
-      );
+  public static void saveCmFile(String fileName) {
+    BufferedWriter bufWriter = null;
+    try {
+      bufWriter = new BufferedWriter(new FileWriter(fileName));
+
+      for (CmTuple cmTuple : cmInfoMap.values()) {
+        try {
+          bufWriter.write(cmTuple.toLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
 
     } catch (
         IOException e) {
       e.printStackTrace();
     } finally {
-      if (file != null) {
+      if (bufWriter != null) {
         try {
-          file.close();
+          bufWriter.close();
         } catch (IOException e) {
         }
       }
@@ -74,21 +76,21 @@ public class CmUtils {
 
   }
 
-  public static void addCmInfo(byte[] cm, byte[] addr_pk, byte[] v, byte[] rho, byte[] r,
-      byte[] s) {
-    CmTuple cmTuple = new CmTuple(cm, addr_pk, v, rho, r, s);
-    cmInfoMap.put(cmTuple.cm, cmTuple);
+  public static void addCmInfo(byte[] cm, byte[] addr_pk, byte[] addr_sk, byte[] v, byte[] rho,
+      byte[] r) {
+    CmTuple cmTuple = new CmTuple(cm, addr_pk, addr_sk, v, rho, r);
+    cmInfoMap.put(cmTuple.getKeyString(), cmTuple);
   }
 
   public static void useCmInfo(byte[] cm) {
-    CmTuple cmTuple = cmInfoMap.get(cm);
+    CmTuple cmTuple = cmInfoMap.get(ByteArray.toHexString(cm));
     cmTuple.used = 0x01;
-    cmInfoMap.put(cm, cmTuple);
+    cmInfoMap.put(ByteArray.toHexString(cm), cmTuple);
   }
 
 
   public static CmTuple getCm(byte[] cm) {
-    return cmInfoMap.get(cm);
+    return cmInfoMap.get(ByteArray.toHexString(cm));
   }
 
 
@@ -98,10 +100,10 @@ public class CmUtils {
     public int caseNum;
     public byte[] cm;
     public byte[] addr_pk;
+    public byte[] addr_sk;
     public byte[] v;
     public byte[] rho;
     public byte[] r;
-    public byte[] s;
     public byte used;
 
     public CmTuple(String line) {
@@ -109,33 +111,42 @@ public class CmUtils {
       String[] x = line.split(":");
       cm = Utils.hexToBytes(x[0]);
       addr_pk = Utils.hexToBytes(x[1]);
-      v = Utils.hexToBytes(x[2]);
-      rho = Utils.hexToBytes(x[3]);
-      r = Utils.hexToBytes(x[4]);
-      s = Utils.hexToBytes(x[5]);
-      used = Utils.hexToBytes(x[7])[0];
+      addr_sk = Utils.hexToBytes(x[2]);
+      v = Utils.hexToBytes(x[3]);
+      rho = Utils.hexToBytes(x[4]);
+      r = Utils.hexToBytes(x[5]);
+      used = (byte) Character.digit(x[6].charAt(0), 16);
     }
 
-    public CmTuple(byte[] cm, byte[] addr_pk, byte[] v, byte[] rho, byte[] r,
-        byte[] s) {
+    public CmTuple(byte[] cm, byte[] addr_pk, byte[] addr_sk, byte[] v, byte[] rho, byte[] r) {
       this.cm = cm;
       this.addr_pk = addr_pk;
+      this.addr_sk = addr_sk;
       this.v = v;
       this.rho = rho;
       this.r = r;
-      this.s = s;
       used = 0x00;
+    }
+
+    public String getKeyString() {
+      return ByteArray.toHexString(cm);
     }
 
     public String toLine() {
       StringBuilder line = new StringBuilder();
       line.append(ByteArray.toHexString(cm));
+      line.append(":");
       line.append(ByteArray.toHexString(addr_pk));
+      line.append(":");
+      line.append(ByteArray.toHexString(addr_sk));
+      line.append(":");
       line.append(ByteArray.toHexString(v));
+      line.append(":");
       line.append(ByteArray.toHexString(rho));
+      line.append(":");
       line.append(ByteArray.toHexString(r));
-      line.append(ByteArray.toHexString(s));
-      line.append((char) used);
+      line.append(":");
+      line.append(used);
       return line.toString();
     }
 
@@ -143,6 +154,25 @@ public class CmUtils {
 
   public static void main(String[] args) {
     //test load/save/add/use
+    //add
+    byte[] cm = {0x0001};
+    byte[] addr_pk = {0x02};
+    byte[] addr_sk = {0x03};
+    byte[] v = {0x04};
+    byte[] rho = {0x05};
+    byte[] r = {0x06};
+    byte used = 0x00;
+    CmUtils.addCmInfo(cm, addr_pk, addr_sk, v, rho, r);
+    //save
+    CmUtils.saveCmFile();
+    //load
+    CmUtils.loadCmFile();
+    //get
+    CmTuple cm1 = CmUtils.getCm(cm);
+    //use
+    CmUtils.useCmInfo(cm);
+
+
   }
 
 
